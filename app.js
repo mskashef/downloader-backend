059484202266208d392a23b67d1827d71a790703
mskashef <mohammadsadeghmk@gmail.com>
2021-01-03 12:33:15 +0330

    v1.2

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ app.use(express.json());
 app.use(express.urlencoded());
 // download('https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.10.1.tar.xz');
 // download('https://cdn.kernel.org/pub/linux/libs/ieee1394/libiec61883-1.0.0.tar.gz');
+function joinBase64Strings(base64Str1, base64Str2) {
+    const bothData = Buffer.from(base64Str1, 'base64').toString('binary')
+        + Buffer.from(base64Str2, 'base64').toString('binary');
+    const joinedBase64Result = Buffer.from(bothData.toString(), 'binary').toString('base64');
+    return joinedBase64Result;
+}
+
 app.get('/', (req, res) => {
     const url = req.query.url;
 
@@ -13,14 +20,19 @@ app.get('/', (req, res) => {
 
     let i = 0;
     const done = () => {
-        if (i === 8)
-            res.status(200).json({chunks: fileChunks, ranges: responseRanges});
+        if (i !== 8) return;
+        const base64 = fileChunks.reduce((total, a) => joinBase64Strings(total, a));
+        res.status(200).json({
+            ranges: responseRanges,
+            name: url.includes('/') ? url.split('/')[url.split('/').length - 1] : url,
+            base64Link: 'data:application/octet-stream;base64,' + base64
+        });
     };
     const download = (link) => {
         axios.head(link).then(res => {
             if (!res.headers['accept-ranges'] || res.headers['accept-ranges'] === 'none')
                 return console.log("No Range Support!\nDownload Failed: This Downloader is just for Parallel Download!!!");
-            console.log("Started Downloading");
+            console.log("Started Downloading : " + url);
             const length = Number(res.headers['content-length']);
             const chunkSize = Math.round(length / 8);
             const ranges = [0, 1, 2, 3, 4, 5, 6, 7].map(i => {
@@ -29,7 +41,7 @@ app.get('/', (req, res) => {
             console.log(ranges);
             responseRanges = ranges;
             ranges.map((range, index) => {
-                console.log("Starting:", index);
+                console.log("Started:", index);
                 axios.get(link, {headers: {'range': range}, responseType: 'arraybuffer'}).then(res => {
                     fileChunks[index] = Buffer.from(res.data, 'binary').toString('base64');
                     // fileChunks[index] = res.data;
